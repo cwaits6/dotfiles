d4a24d075431bff8a5aa32c3da1f9f780a22f51a
Cody Waits <137742107+cwaits6@users.noreply.github.com>
2025-12-11 22:18:27 -0500

    feat: add floating terminal and custom splash screen ascii

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      -- File picker options
       picker = {
         sources = {
           explorer = {
@@ -9,6 +10,43 @@ return {
             auto_close = true,
           },
         },
+      },
+      -- Enable floating terminal
+      terminal = {
+        enabled = true,
+        win = {
+          style = "float",
+          border = "rounded",
+          width = 0.9,
+          height = 0.9,
+        },
+      },
+      -- Custom splash screen logo
+      dashboard = {
+        preset = {
+          header = [[
+        ██████   █████ ██████████    ███████    █████   █████ █████ ██████   ██████
+      ░░██████ ░░███ ░░███░░░░░█  ███░░░░░███ ░░███   ░░███ ░░███ ░░██████ ██████ 
+        ░███░███ ░███  ░███  █ ░  ███     ░░███ ░███    ░███  ░███  ░███░█████░███ 
+        ░███░░███░███  ░██████   ░███      ░███ ░███    ░███  ░███  ░███░░███ ░███ 
+        ░███ ░░██████  ░███░░█   ░███      ░███ ░░███   ███   ░███  ░███ ░░░  ░███ 
+        ░███  ░░█████  ░███ ░   █░░███     ███   ░░░█████░    ░███  ░███      ░███ 
+        █████  ░░█████ ██████████ ░░░███████░      ░░███      █████ █████     █████
+      ░░░░░    ░░░░░ ░░░░░░░░░░    ░░░░░░░         ░░░      ░░░░░ ░░░░░     ░░░░░ 
+
+
+              ]],
+        },
+      },
+    },
+    -- Assign keys to open terminal
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("snacks").terminal()
+        end,
+        desc = "Toggle Snacks Terminal",
       },
     },
   },
