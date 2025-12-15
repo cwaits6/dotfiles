dab3c254e2fc3bff3ea84caf3e71381888d4630c
Cody Waits <137742107+cwaits6@users.noreply.github.com>
2025-12-15 10:30:40 -0500

    feat(nvim): move code blocks with shift j and k

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
--- Deleted items go to d register instead of unamed register
+
+-- Move code blocks with Shift J+K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Have deletions go to the d register instead of unamed register
 vim.keymap.set("n", "d", '"dd', { noremap = true })
 vim.keymap.set("v", "d", '"dd', { noremap = true })
 vim.keymap.set("n", "D", '"dD', { noremap = true })
 vim.keymap.set("v", "D", '"dD', { noremap = true })
 
--- Changed items go to c register instead of unamed register
+-- Have changes go to the c register instead of unamed register
 vim.keymap.set("n", "c", '"cc', { noremap = true })
 vim.keymap.set("v", "c", '"cc', { noremap = true })
 vim.keymap.set("n", "C", '"cC', { noremap = true })
